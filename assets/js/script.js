7e0b65e05e79911d2e4d98fc48f8b5c99498e894
DanielRubi0 <drubiogarcia@gmail.com>
2022-10-18 19:31:48 +0200

    second commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Ejercicio 2
+console.log("¡Empezamos con javascript!");
+
+//Ejercicio 3
+console.log("Hola, soy un estudiante Hackademy.");
+
+//Ejercicio 4
+let nombre = "Daniel";
+nombre = "Dani";
+console.log(nombre);
+
+//Ejercicio 5
+let frase = " está aprendiendo JavaScript";
+console.log(nombre + frase);
