619434d4954a2bb8b379e314aafa6a472116bb11
DanielRubi0 <drubiogarcia@gmail.com>
2022-10-25 19:14:24 +0200

    23 corregido

--- js/Ejercicio23.js
+++ js/Ejercicio23.js
@@ -1,10 +1,26 @@
 //Ejercicio 23: Crear un programa que calcule el factorial de un número n, donde el valor de n se recibe desde el “prompt” y además se verifique si el valor es de tipo number. Si no lo es, se sale del programa con un mensaje de error.
 let n = Number(prompt('Elige un numero para calcular su factorial'));
-if (isNaN(n)) {
-    console.log("Error. Tienes que introducir un número");
+if (typeof n === "number" && !Number.isNaN(n)) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++){
+    resultado = resultado * i;}
+    alert(`El factorial de ${n} es: ${resultado}`);
 } 
-let resultado = 1;
-let i = 1;
-for (let i = 1; i <= n; i++){
-resultado = resultado * i;}
-alert(`El factorial de ${n} es: ${resultado}`);
+
+else {
+    alert("Error. Tienes que introducir un número");
+}
+
+// //Opción con do while
+// let fact = Number(prompt('Elige un numero para calcular su factorial'));
+// let i = 1;
+// let total2 = 1;
+//     if (typeof fact === "number" && !Number.isNaN(fact)) {
+//         do {
+//             console.log(total2 = total2 * i);
+//             i++;
+//         } while (i<= fact)}
+
+//     else{
+//         console.log("No es un número");
+//     }
---
+++ js/Ejercicio24.js
@@ -0,0 +1,3 @@
+//Ejercicio 24: Escribir una función que, dados un mínimo y un máximos,cree un número aleatorio entre un número y un máximo.
+
+//Usar math.random
