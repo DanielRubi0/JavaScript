b2da46a399d50c1f459ef15175e4c7ba56dd46f7
DanielRubi0 <drubiogarcia@gmail.com>
2022-10-27 20:27:53 +0200

    32 corregido

--- js/Ejercicio32.js
+++ js/Ejercicio32.js
@@ -1,11 +1,13 @@
 //Ejercicio 32: Calcular la serie de fibonacci que devuelve hasta n números bajo forma de array. Ejemplo [1, 2, 3, 5, 8, 13, 21, 34]
 
 function fibonacci(n) {
-    let fib=[0,1];
+    const fib=[0,1];
 
-for (let i=2; i < n; i++){
+    for (let i=2; i <= n; i++){
     fib[i]=fib[i-1] + fib[i-2];
 }
+
 return fib
 }
+
 console.log(fibonacci(10));
